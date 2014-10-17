3baf92a4005d347f06333b14a1e75118b9e141f8
Aaron <a@aaronfisher.co>
2014-10-17 17:17:24 +0100

    Words

--- importer.php
+++ importer.php
@@ -10,20 +10,20 @@ if ($_FILES["file"]["type"] == "text/xml" && getExtension($_FILES["file"]["name"
 	echo "Error: " . $_FILES["file"]["error"] . "<br>";
 } else {
 	if (file_exists($_FILES["file"]["tmp_name"])) {
-	    $import = simplexml_load_file($_FILES["file"]["tmp_name"]);
+	    	$import = simplexml_load_file($_FILES["file"]["tmp_name"]);
 	 
 		if($import->body->outline){
 			foreach($import->body->outline as $feedtop){
 				if(!$feedtop->outline) {
 					//top level feeds
-					echo($feedtop["title"]);// This is to extract the title of each entry
+					echo($feedtop["title"]);// Extract the title of each entry
 					echo"<br>";
-					echo($feedtop["xmlUrl"]);// This is to extract the url from each entry
+					echo($feedtop["xmlUrl"]);// Extract the url from each entry
 					echo"<br><br>";
 				} else {
 					foreach($feedtop->outline as $feed){
-						//this part should be able to display any feeds that are buries within a folder
-						//have not tested this at the moment but I believe it worked when last tested with a Google Reader export
+						//this part should be able to display any feeds that are buried within a folder
+						//untested but should work
 						echo($feed["title"]);
 						echo"<br>";
 						echo($feed["xmlUrl"]);
@@ -35,7 +35,8 @@ if ($_FILES["file"]["type"] == "text/xml" && getExtension($_FILES["file"]["name"
 
 		
 	} else {
-	    exit('Failed to open'.$_FILES["file"]["name"]);
+		// No file exists
+	    	exit('Failed to open'.$_FILES["file"]["name"]);
 	}
 }
 ?>
